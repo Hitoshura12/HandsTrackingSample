13f889c090bd7e9e0afc5ecc50fd56d08ad31575
Hitoshura12 <sgoungaf@gmail.com>
2023-08-03 21:08:22 +0100

    first comm

---
+++ Source/HandsTrainSample/HandsTrainSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HandsTrainSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HANDSTRAINSAMPLE_API AHandsTrainSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
